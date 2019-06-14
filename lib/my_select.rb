5e6558bd5f660bda2a1b0b6b17ec7cb34f9918c3
Hannah N <nilagathp@gmail.com>
2019-06-14 07:17:23 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
-end
+def my_select(array)
+ if block_given?
+   index = 0
+   new_array = []
+   while index < array.size
+    if yield(array[index]) == true
+      new_array << (array[index])
+    end
+   index+= 1
+   end
+   return new_array
+  end
