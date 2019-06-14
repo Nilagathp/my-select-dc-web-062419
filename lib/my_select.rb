4dfdfcb036f1730dc953921df129c1ae8105cf6b
Hannah N <nilagathp@gmail.com>
2019-06-14 07:18:32 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,10 +3,11 @@ def my_select(array)
    index = 0
    new_array = []
    while index < array.size
-   if yield(array[index]) == true
+    if yield(array[index]) == true
       new_array << (array[index])
-      end
-      index+= 1
-      end
-      return new_array
-      end
+    end
+   index+= 1
+   end
+   return new_array
+  end
+end
